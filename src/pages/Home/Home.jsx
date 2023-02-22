ad989ac8af8f6a941c94589be64397da1da75194
cfx78 <104265674+cfx78@users.noreply.github.com>
2023-02-21 20:17:23 -0600

    Trying new direction for site

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,40 +1,22 @@
 import './Home.scss'
-import { motion as m } from 'framer-motion'
+
 function Home() {
     return (
-        <m.div
-            initial={{ width: 0 }}
-            animate={{ width: '100%' }}
-            transition={{ duration: 1, ease: 'backInOut' }}
-            exit={{ x: '100%' }}
-            className="min-w-full h-full bg-accent-700 absolute top-0 left-0"
-        >
-            <header className="text-center">
-                <h1 className="text-9xl text-primary-200 pt-16">Welcome</h1>
-                <div className="pt-36 text-primary-50 text-4xl">
-                    <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. In debitis doloremque ab mollitia, earum nesciunt
-                        eius iure magni praesentium vitae fugiat porro quae.
-                        Enim consectetur facilis doloremque ad, et eius!
-                    </p>
-                    <p>
-                        Repellat sapiente ipsum sequi optio inventore ab
-                        distinctio, explicabo atque animi earum, itaque
-                        praesentium tempora ex perferendis nihil quos nesciunt
-                        repellendus enim aut quidem odit vero adipisci? Eveniet,
-                        incidunt harum.
-                    </p>
-                    <p>
-                        Sed consectetur delectus ad voluptate obcaecati! Beatae
-                        tempora, consectetur magni ipsum odio architecto nobis
-                        facere accusantium. Dolor tenetur nihil nostrum
-                        reprehenderit. Nihil voluptatibus eaque magnam nam
-                        voluptatum ipsum pariatur fugiat.
-                    </p>
-                </div>
-            </header>
-        </m.div>
+        <section className="grid " id="Home">
+            <div className="max-w-max mt-24 p-4 ml-3 flex flex-col gap-2 intro">
+                <h1 className="text-8xl text-start">
+                    Hi, I'm <span className="name">Cortez Foxx</span> <br />
+                </h1>
+                <br />
+                <h2 className="text-4xl text-end underline underline-offset-4 decoration-[#06bcc1]">
+                    Front-End Developer
+                </h2>
+            </div>
+
+            <div className="logo row-end-3 col-end-10 mr-36 ">
+                <img className="w-80" src="../../src/assets/Avatar.svg" />
+            </div>
+        </section>
     )
 }
 
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,17 +1,5 @@
-import React from 'react'
-import { motion as m } from 'framer-motion'
 const Projects = () => {
-    return (
-        <m.div
-            initial={{ width: 0 }}
-            animate={{ width: '100%' }}
-            transition={{ duration: 1, ease: 'backInOut' }}
-            exit={{ x: '100%' }}
-            className="min-w-full h-full bg-info-500 "
-        >
-            Projects
-        </m.div>
-    )
+    return <section id="Projects">Projects</section>
 }
 
 export default Projects
